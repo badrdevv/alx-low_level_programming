57f000fa9234cf9d8fb1eaf1aa3799e28c38702e
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-16 11:15:08 -0700

    Fixed a error at the output of the file!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,11 @@ int main(void)
 	long int k;
 	long long int l;
 	float m;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(i));
-	printf("Size of a char: %lu byte(s)\n", sizeof(j));
-	printf("Size of a char: %lu byte(s)\n", sizeof(k));
-	printf("Size of a char: %lu byte(s)\n", sizeof(l));
-	printf("Size of a char: %lu byte(s)\n", sizeof(m));
+	printf("Size of an int: %lu byte(s)\n", sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", sizeof(m));
 	return (0);
 }
