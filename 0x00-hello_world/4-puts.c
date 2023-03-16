bcbdb4916625bb92011c8c0fd33c629c07cdc453
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-16 10:41:49 -0700

    Fixed a bug + add a documentation inside the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * I used the puts function, to print exactly
+ * that sentence with the double quote at the first treated as normal character
+ * not as special one since we used a escape
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
