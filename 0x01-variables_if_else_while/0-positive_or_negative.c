26af5219a57bf24cb5760d65145ea5c9500afecd
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-17 13:01:21 -0700

    Fixed a bug inside 0-positive_or_negative.c file!

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * main - this is my main function that check if n is positive or negative or equal to zero
+ * main - this is my main function that check if n is positive or negative
+ * or equal to zero
  *
  * Return: Always 0
  */
@@ -18,16 +19,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-       	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
