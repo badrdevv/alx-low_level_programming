5e8a3094e78a55a2a083c43f6bd04558b06c52aa
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-17 15:36:30 -0700

    Add 7-print_tebahpla.c file, it contains a script that print the lowercase alphabet in reverse, followed by a new line, putchar function is used

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - my main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
