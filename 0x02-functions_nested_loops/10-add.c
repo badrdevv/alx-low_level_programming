c5dbf325e6a39ab3dc861f4618077dbbc19de8a1
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-21 10:28:51 -0700

    Add 10-add.c file, it contains a script that defile the add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a function that give the result of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the result of a and b
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
