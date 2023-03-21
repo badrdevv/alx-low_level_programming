44307b1ac6de3e59ff62b711aba5a626be292b12
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-21 08:15:37 -0700

    Add 3-islower.c file, it contains a script that define the function _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _islower - a function that detect lowercase characters
+ *
+ * Return: 1 if the character is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
