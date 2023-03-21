a91fd57ab2258f1e84e9ddcad1e3b0b8974ec1e2
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-21 09:01:44 -0700

    Add 4-isalpha.c file, it contains a script that define the function _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function check whether a letter is lowercase
+ * or uppercase
+ * @c: alphabet character
+ *
+ * Return: 1 if c is lowercase or uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
