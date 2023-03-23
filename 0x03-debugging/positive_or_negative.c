def9c5282454e2334dff00805796775100c3c197
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-23 07:10:28 -0700

    testing something

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,6 +12,8 @@ void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
+	else if (i == 0)
+		printf("%d is equal to zero\n", i);
 	else
 		printf("%d is positive\n", i);
 }
