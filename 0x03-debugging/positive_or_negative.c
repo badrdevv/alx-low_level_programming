ca5902723a6d567df06036a2a65243bb7ae9a5cf
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-23 07:15:57 -0700

    still figuring out the problem inside 0-main.c file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -13,7 +13,7 @@ void positive_or_negative(int i)
 	if (i < 0)
 		printf("%d is negative\n", i);
 	else if (i == 0)
-		printf("%d is equal to zero\n", i);
+		printf("%d is zero\n", i);
 	else
 		printf("%d is positive\n", i);
 }
