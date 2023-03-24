db80755320b5a9a6fd3c9936a8d63ebc22426697
badrdevv <badreddinebouroumiya@gmail.com>
2023-03-23 17:42:06 -0700

    Add 2-mul.c file, it contains a script that define the mul function, add 2-main.c for testing this function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of those two integers
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
