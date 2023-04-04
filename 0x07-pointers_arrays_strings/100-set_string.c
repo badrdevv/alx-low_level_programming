5e21377b90ac5a2c90ffc61f6d93bb5c57a48f03
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-03 20:08:11 -0700

    Add 100-set_string.c file, it contains a script that define the set_string function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - my function inside this file
+ * @s: pointer to pointer
+ * @to: pointer char
+ *
+ * Return: nothing returned
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
