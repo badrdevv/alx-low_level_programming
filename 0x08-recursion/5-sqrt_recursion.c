b0557b8ccebc55a37e0e9d3780350eabf46c5eca
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-05 07:23:04 -0700

    Add 5-sqrt_recursion.c file, it contains a script that define the _sqrt_recursion function, note that i created another function called true_sqrt_recursion function inside of _sqrt_recursion function to search for the natural number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a function that returns the
+ * natural square root of a number
+ * @n: the number to calculate the square root
+ *
+ * Return: the result of square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * true_sqrt_recursion - this function is used to recurse to
+ * find the natural square root of a number
+ * @n: the number to calculate the square root
+ * @b: the iterator used in this function
+ *
+ * Return: the result of square root
+ */
+
+int true_sqrt_recursion(int n, int b)
+{
+	if (b * b > n)
+		return (-1);
+	if (b * b == n)
+		return (i);
+
+	return (true_sqrt_recursion(n, b + 1));
+}
