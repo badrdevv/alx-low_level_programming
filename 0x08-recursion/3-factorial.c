43a339927db94fabbe9a992fc4134042a9074b44
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-05 06:58:36 -0700

    Add 3-factorial.c file, it contains a script that define the factorial function

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - my function inside this file
+ * @n: the integer number given by user
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+
+	return (n * factorial(n-1));
+}
