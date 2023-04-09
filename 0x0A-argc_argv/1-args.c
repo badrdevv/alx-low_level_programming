df0489c4d7a7f6db1659aa914603e12d63f0d9e1
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-09 11:52:12 -0700

    add 1-args.c file, it is a C source code file that prints the number of arguments passed into it, the program will print a number, followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - my main function
+ * @argc: number of arguments
+ * @argv: array of strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
