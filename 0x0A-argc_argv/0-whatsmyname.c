7450bfaf8a056c366963c495720c3289b3f7a728
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-09 11:36:03 -0700

    Add main.h header file, contains all functions prototypes used in this project, add README.md file, add 0-whatsmyname.c file, it is a C program that prints the program's name, even if the program name is changed, it will print the new name without having to recompile it

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the point starter function
+ * @argc: the number of arguments passed to the program
+ * @argv: array of strings, each string is one of
+ * the arguments that was passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+A README.md file, please if you want to know the description of this project and what each file script do, please reach to my github account : badrdevv, happy coding!
