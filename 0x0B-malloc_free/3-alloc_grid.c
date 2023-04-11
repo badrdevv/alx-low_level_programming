662d903c6b52cf4894800b4d2cddf94eca00c627
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-11 11:02:21 -0700

    Add 3-alloc_grid.c file, it contains a C source code file that define the alloc_grid function

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - a nested loop
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2th dim. array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **y;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	y = malloc(sizeof(int *) * height);
+
+	if (y == NULL)
+		return (NULL);
+
+	for (a = 0; a < height; a++)
+	{
+		y[a] = malloc(sizeof(int) * width);
+
+		if (y[a] == NULL)
+		{
+			for (; a >= 0; a--)
+				free(y[a]);
+
+			free(y);
+
+			return (NULL);
+		}
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			mee[a][b] = 0;
+	}
+
+	return (y);
+}
