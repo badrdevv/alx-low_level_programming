8f7eaab4a1f4aff1dc7a24bff3ba785682fe2763
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-11 10:39:40 -0700

    Final try

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	
 	arr = malloc(size * sizeof(char));
-	if (size == 0 || str == NULL)
+	if (size == 0 || arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
