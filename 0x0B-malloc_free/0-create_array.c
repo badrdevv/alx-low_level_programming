a0a05433a7e14e8678f59f0af4d9c81c5f8de2da
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-11 10:38:45 -0700

    searching for bugs in 0-create_array.c file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,9 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
-
-	*arr = malloc(size * sizeof(char));
-
-	if (arr == NULL)
+	
+	arr = malloc(size * sizeof(char));
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
