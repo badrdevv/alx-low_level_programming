65235f1d90eb11647da64ace57947943ebed9d33
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-11 10:49:41 -0700

    Add 1-strdup.c file, it is a C source code file that define the _strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strdup - a function that duplicate to new
+ * memory space location
+ * @str: the character
+ *
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int j, k = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+		i++;
+
+	a = malloc(sizeof(char) * (j + 1));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (k = 0; str[k]; k++)
+		a[k] = str[k];
+
+	return (a);
+}
