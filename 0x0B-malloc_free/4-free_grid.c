be15e41815b76c25140854cf3199f583a79e85c9
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-11 11:05:29 -0700

    Add 4-free_grid.c file, it contains a C source code file that define the free_grid function

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int g;
+
+	for (g = 0; g < height; g++)
+	{
+		free(grid[g]);
+	}
+	free(grid);
+}
