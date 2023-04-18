177b1cb414d33d7015d9191e9eb575eb591584ec
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-17 18:48:14 -0700

    Add 2-main.c file, it contains C source code file that print the name of the file that the program was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - my main function that print the
+ * current compiled file name
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
