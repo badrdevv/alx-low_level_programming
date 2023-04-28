a1ef8d2a5e42a9f27f4d5341bd5d11f955a723b6
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-27 18:16:34 -0700

    Add 1-list_len.c file, it is a C source code file that define the function list_len that returns the number of elements in a linked list_t list, add 1-main.c file to test this function, a [b] file which is a compiled executable file using 1-main.c and 1-list_len.c file, add the list_len function prototype inside the lists.h header file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *t = h;
+
+	while (t != NULL)
+	{
+		count++;
+		t = t->next;
+	}
+	
+	return (count);
+}
