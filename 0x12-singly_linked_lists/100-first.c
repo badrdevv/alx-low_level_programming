62ff8ab94b846fd4977e5ae2256ae71bd49c8f2b
badrdevv <badreddinebouroumiya@gmail.com>
2023-04-27 19:39:06 -0700

    Give execution permissions to all files used here to do my ALX tasks, add 100-first.c file, it contains a C source code file that define the first_function function, add this function prototype inside lists.h header file, add 100-main.c file to test this function, there is a [first] file, it is a compiled executable file using 100-main.c and 100-first.c files

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * first_function - a function that print a sentence before
+ * main function is executed
+ *
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) first_function()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
