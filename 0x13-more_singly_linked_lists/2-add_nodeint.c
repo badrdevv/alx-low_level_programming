952a7f2f9bbdca24132b75b767640f5d1e459030
badrdevv <badreddinebouroumiya@gmail.com>
2023-05-02 18:22:16 -0700

    fix my betty style in 2-add_nodeint.c file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 
 /**
- * add_noteint - a function that add a new node at the beginning
+ * add_nodeint - a function that add a new node at the beginning
  * of a linked list
  * @head: a pointer to the first node in the list
  * @n: the data to insert in new node
  *
  * Return: the pointer to the new node, otherwise NULL
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *v;
