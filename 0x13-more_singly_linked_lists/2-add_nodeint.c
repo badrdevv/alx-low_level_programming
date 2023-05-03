f280089e554774a02f5cc8248c6974694082f969
badrdevv <badreddinebouroumiya@gmail.com>
2023-05-02 18:20:22 -0700

    Add 2-add_nodeint.c file, it is a C source code file that define the add_nodeint function, add 2-main.c to test this function, [c] compiled executable file to check the output

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_noteint - a function that add a new node at the beginning
+ * of a linked list
+ * @head: a pointer to the first node in the list
+ * @n: the data to insert in new node
+ *
+ * Return: the pointer to the new node, otherwise NULL
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *v;
+
+	v = malloc(sizeof(listint_t));
+	if (!v)
+		return (NULL);
+
+	v->n = n;
+	v->next = *head;
+	*head = v;
+
+	return (v);
+}
